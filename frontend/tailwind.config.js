3df3d7f9deb143b57a46f1315bdf3c6902f83df7
vicentillu <vicens.delgado.torrens@gmail.com>
2025-08-24 22:11:08 +0200

    style(frontend): ajustes en estilos y configuración de Tailwind

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./pages/**/*.{js,jsx,ts,tsx}",      // Pages Router
-    "./components/**/*.{js,jsx,ts,tsx}",
-    "./app/**/*.{js,jsx,ts,tsx}",        // (si tuvieras App Router)
+    "./src/**/*.{js,jsx,ts,tsx}"
   ],
   theme: {
     extend: {},
